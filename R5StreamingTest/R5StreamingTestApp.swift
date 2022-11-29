b8da952e509abc2e99f26822cd768fac05f7f34f
Alexander Ostrovsky <thealeos@gmail.com>
2022-11-29 16:36:16 +0400

    Create a new Xcode project

---
+++ R5StreamingTest/R5StreamingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  R5StreamingTestApp.swift
+//  R5StreamingTest
+//
+//  Created by Alexander Ostrovsky on 29.11.22.
+//
+
+import SwiftUI
+
+@main
+struct R5StreamingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
